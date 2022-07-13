842a4e9bf44995055f6ef5860f76bd596e57ad0d
Quang Minh Pham <quangminh1575@gmail.com>
2022-07-13 15:39:59 +0200

    updated

---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Articles;
+
+class ArticleFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 1; $i <= 10; $i++) {
+            $article = new Articles();
+            if ($i % 2) {
+                $article->setNom("Montre électronique n° $i");
+                $article->setDescription("Description de la montre n° $i");
+                $article->setPrix($i * 10);
+                $article->setNombresEnStock($i);
+                $article->setIdCategorie(1);
+                $article->setCreateAt(new \DateTimeImmutable());
+            }else{
+                $article->setNom("Balace électronique n° $i");
+                $article->setDescription("Description de la balance n° $i");
+                $article->setPrix($i * 11);
+                $article->setNombresEnStock($i);
+                $article->setIdCategorie(2);
+                $article->setCreateAt(new \DateTimeImmutable());
+            }
+            $manager->persist($article);
+        }
+
+        $manager->flush();
+    }
+}
---
+++ src/DataFixtures/CategorieFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Categories;
+
+class CategorieFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $first_categorie = new Categories();
+        $first_categorie->setNom("Bijoux");
+        $first_categorie->setNombreProduits(250);
+        $manager->persist($first_categorie);
+
+        $second_categorie = new Categories();
+        $second_categorie->setNom("Electroménager");
+        $second_categorie->setNombreProduits(330);
+        $manager->persist($second_categorie);
+        $manager->flush();
+    }
+}
