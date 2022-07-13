c0f0f27282659df1a4f83cbf275ce974cdd1b21f
Quang Minh Pham <quangminh1575@gmail.com>
2022-07-13 14:07:59 +0200

    first commit

---
+++ src/Entity/Articles.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticlesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticlesRepository::class)
+ */
+class Articles
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $prix;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $create_at;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nombres_en_stock;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $id_categorie;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrix(): ?int
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(int $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getCreateAt(): ?\DateTimeImmutable
+    {
+        return $this->create_at;
+    }
+
+    public function setCreateAt(\DateTimeImmutable $create_at): self
+    {
+        $this->create_at = $create_at;
+
+        return $this;
+    }
+
+    public function getNombresEnStock(): ?int
+    {
+        return $this->nombres_en_stock;
+    }
+
+    public function setNombresEnStock(int $nombres_en_stock): self
+    {
+        $this->nombres_en_stock = $nombres_en_stock;
+
+        return $this;
+    }
+
+    public function getIdCategorie(): ?int
+    {
+        return $this->id_categorie;
+    }
+
+    public function setIdCategorie(int $id_categorie): self
+    {
+        $this->id_categorie = $id_categorie;
+
+        return $this;
+    }
+}
