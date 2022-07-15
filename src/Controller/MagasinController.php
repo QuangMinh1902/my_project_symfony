f14348adae10ac3fe8f8d47b282bd355028faf44
Quang Minh Pham <quangminh1575@gmail.com>
2022-07-15 09:31:24 +0200

    updated

--- src/Controller/MagasinController.php
+++ src/Controller/MagasinController.php
@@ -8,8 +8,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Articles;
-use App\Entity\Categories;
-use App\Entity\Users;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\ArticleType;
 
